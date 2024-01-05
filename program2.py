e9a738b3f0f8646cf9630edd03df823356a86409
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-01-05 13:46:16 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,24 @@
-class Solution(object):
+class Solution():
+   
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        i = 0
+        result = 0
+  
+        while i < len(s):
+            if i+1 < len(s) and s[i:i+2] in roman:
+                result += roman[s[i:i+2]]
+                i += 2
+            else:
+                result += roman[s[i]]
+                i += 1
+        return result
+
+inputvalue = input("Enter the roman : ")
+# creating an instance of the Solution class
+solution = Solution()
+# call the romanToInt method with a string argument that will be taken as input from user
+result = solution.romanToInt(inputvalue)
+print(result)
+
+
