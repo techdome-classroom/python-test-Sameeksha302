80651478a11f06461302d62ca6d756c46b41e5b9
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-01-05 13:38:01 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,12 +1,24 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+class Solution():
+    def isValid(self,s):
+        stack = []
+        for ch in s:
+            if ch in ('(', '{', '['):
+                stack.append(ch)
+            else:
+                if stack and ((stack[-1] == '(' and ch == ')') or
+                            (stack[-1] == '{' and ch == '}') or
+                            (stack[-1] == '[' and ch == ']')):
+                    stack.pop()
+                else:
+                    return False
+        return not stack
 
+exp = input("Enter the expression : ")
+# creating an instance of the Solution class
+solution = Solution()
+# call the isValid method with a string argument that will be taken as input from user
+result = solution.isValid(exp)
+print(result)
 
 
   
